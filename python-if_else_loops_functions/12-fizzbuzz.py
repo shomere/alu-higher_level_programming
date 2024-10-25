c302859e1e54ecb78d3a2a9e52f965baf48c673c
shomere <s.homere@alustudent.com>
2024-10-25 09:18:08 -0700

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-def pow(a, b):
-    return a ** b
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            i = "FizzBuzz"
+        elif i % 5 == 0:
+            i = "Buzz"
+        elif i % 3 == 0:
+            i = "Fizz"
+
+        print(i, end=" ")
