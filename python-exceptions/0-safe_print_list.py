4de05aabbc9c959e6c9861f156af60d119cb3044
shomere <s.homere@alustudent.com>
2024-11-06 01:11:16 -0800

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
@@ -9,5 +10,4 @@ def safe_print_list(my_list=[], x=0):
     finally:
         print()  # Print a new line after all elements
     return count
-
 
