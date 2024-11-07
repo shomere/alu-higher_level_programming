a497205312b2c29dec76a4248844c54a7f301244
shomere <s.homere@alustudent.com>
2024-11-07 03:40:16 -0800

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a square"""
+
+
+class Square:
+    """Represents a square with a private instance attribute: size"""
+
+    def __init__(self, size):
+        """Initializes the square with a private size attribute"""
+        self.__size = size
+
