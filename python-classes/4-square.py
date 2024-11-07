6b0ecb7e60b1eb83722dea7a02f5f6c70ee76b38
shomere <s.homere@alustudent.com>
2024-11-07 07:22:38 -0800

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,27 +3,28 @@
 
 
 class Square:
-    """Represents a square with a private instance attribute: size"""
+    """Creates a square.
+    Private instance attributes: size
+    """
 
     def __init__(self, size=0):
-        """Initializes the square with a private size attribute"""
-        self.size = size  # Calls the setter to apply validation
+        """Initialize data."""
+        self.__size = size
 
     @property
     def size(self):
-        """Retrieves the size of the square"""
+        """Retrieve size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the size of the square with type and value checks"""
+        """Sets size to a value."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """Calculates and returns the current square area"""
+        """Returns square area."""
         return self.__size ** 2
-
