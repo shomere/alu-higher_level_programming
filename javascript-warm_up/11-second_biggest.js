aa415ecc57372d2bd64ac70bd672f10bfaee64ad
shomere <s.homere@alustudent.com>
2025-01-31 07:31:04 -0800

    11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2).map(Number);
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  args.sort((a, b) => b - a); // Sort in descending order
+  console.log(args[1]); // Second largest number
+}
+
