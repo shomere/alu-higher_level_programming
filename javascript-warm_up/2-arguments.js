aa36a834f944b7c152dd1d87e06d6961038420e3
shomere <s.homere@alustudent.com>
2025-01-31 12:27:14 -0800

    2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const argscount = process.argv.length - 2;
+const args = process.argv.slice(2);
 
-if (argscount === 0){
-	console.log('No Argument');
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-else if (argscount === 1){
-	console.log('Argument found');
-}
-else {
-	console.log('Arguments found');
-}
+
