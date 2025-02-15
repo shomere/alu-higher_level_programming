f31febad72d4e0df34f5760c079b96b96dddaa54
shomere <s.homere@alustudent.com>
2025-02-15 06:23:04 -0800

    1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
+
 const fs = require('fs');
+const file = process.argv[2];
+const write_ = process.argv[3];
 
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.log(err);
+fs.writeFile(file, write_, 'utf8', function (error) {
+  if (error) {
+    console.log(error);
   }
 });
